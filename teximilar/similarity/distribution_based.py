1843d65138572cebe533e4b2ce832ca2652d6a9f
吴子豪 <vortez.wohl@gmail.com>
2025-04-14 17:09:20 +0800

    feat: 添加js散度算子

--- teximilar/similarity/distribution_based.py
+++ teximilar/similarity/distribution_based.py
@@ -1,7 +1,16 @@
 # 越小越相似
 import numpy as np
 
+from teximilar.similarity.set_based import bias
+
 
 def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
-    q = np.where(q == 0, 1e-10, q)
+    q = np.where(q == 0, bias, q)
+    p = p / np.sum(p)
+    q = q / np.sum(q)
     return np.sum(p * np.log(p / q))
+
+
+def js_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
+    m = (p + q) / 2
+    return (kl_divergence(p, m) + kl_divergence(q, m)) / 2
