e816ae88ba8ebbf068720dc1179db809251728c6
吴子豪 <vortez.wohl@gmail.com>
2025-04-14 16:38:58 +0800

    feat: 添加相似性算子

---
+++ teximilar/similarity/vector_based.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def l2_normalize(x: np.ndarray) -> np.ndarray:
+    return x / np.sqrt(np.sum(np.multiply(x, x)))
+
+
+def z_score_normalize(x: np.ndarray) -> np.ndarray:
+    mean = np.mean(x)
+    std_dev = np.std(x)
+    return (x - mean) / std_dev
+
+
+def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
+    if not isinstance(p, np.ndarray):
+        p = np.asarray(p)
+    if not isinstance(q, np.ndarray):
+        q = np.asarray(q)
+    q = np.where(q == 0, 1e-10, q)
+    return np.sum(p * np.log(p / q))
+
+
+def euclidean_similarity(p: np.ndarray, q: np.ndarray) -> np.float32:
+    distance = p - q
+    distance = np.sum(np.multiply(distance, distance))
+    return np.sqrt(distance)
+
+
+def cosine_similarity(p: np.ndarray, q: np.ndarray) -> np.float32:
+    a = np.matmul(np.transpose(p), q)
+    b = np.sum(np.multiply(p, p))
+    c = np.sum(np.multiply(q, q))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+
+
+def chebyshev_similarity(p: np.ndarray, q: np.ndarray) -> np.float32:
+    return np.max(np.abs(p - q))
